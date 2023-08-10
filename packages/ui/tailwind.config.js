cdeefc18db9541430abd67ea74f600ae35a80041
slekup <74949101+slekup@users.noreply.github.com>
2023-08-11 00:10:12 +1200

    fix: tailwind config

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,6 +2,7 @@ const sharedConfig = require('tailwind-config/tailwind.config.js');
 
 module.exports = {
   // prefix ui lib classes to avoid conflicting with the app
+  content: ['src/**/*.{js,ts,jsx,tsx}'],
   prefix: 'ui-',
   presets: [sharedConfig],
 };
