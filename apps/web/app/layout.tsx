a0ba0600b18439e40e22faa32130d6b7e31cf27f
slekup <74949101+slekup@users.noreply.github.com>
2023-08-10 23:57:08 +1200

    chore(build): setup tailwindcss

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,13 @@
+import 'ui/styles.css';
+import '../styles/globals.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-zinc-900">
       <body>{children}</body>
     </html>
   );
