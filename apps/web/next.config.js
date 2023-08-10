6bb05baf457b3af6f8c75058955a0b693d9ffc82
slekup <74949101+slekup@users.noreply.github.com>
2023-08-10 20:53:50 +1200

    chore: init monorepo with turborepo

--- apps/web/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/app',
-  reactStrictMode: true,
-  transpilePackages: ['@odnlabs/ui', '@odnlabs/utils'],
-};
-
-module.exports = nextConfig;
