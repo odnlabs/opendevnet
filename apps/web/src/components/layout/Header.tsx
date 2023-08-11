4679d4a2ff7c2c172a9bfaec2adcb426ca9bf35e
slekup <74949101+slekup@users.noreply.github.com>
2023-08-12 02:09:09 +1200

    style: navigation bar default buttons

--- apps/web/src/components/layout/Header.tsx
+++ apps/web/src/components/layout/Header.tsx
@@ -1,7 +1,11 @@
 export const Header: React.FC = () => {
   return (
     <>
-      <div className="fixed top-0 left-0 w-full h-10 border-b bg-header border-border"></div>
+      <div className="fixed top-0 left-0 w-full h-10 border-b bg-header border-border">
+        <div className="flex">
+          <p className="px-3 py-2 font-bold text-text-faint">Open Dev Net</p>
+        </div>
+      </div>
     </>
   );
 };
--- apps/web/src/components/layout/NavigationBar.tsx
+++ apps/web/src/components/layout/NavigationBar.tsx
@@ -1,9 +1,72 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { IconType } from 'react-icons';
+import { FaCompass } from 'react-icons/fa';
+import { HiHome, HiPlus } from 'react-icons/hi';
+
+interface DefaultButton {
+  label: string;
+  route: string;
+  icon: IconType;
+}
+
+const NavButton: React.FC<{ btn: DefaultButton }> = ({ btn }) => {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={btn.route}
+      className={`block w-12 h-12 p-3 mt-2 transition duration-200 rounded-3xl ${
+        pathname === btn.route
+          ? 'text-text-button bg-primary'
+          : 'text-primary bg-background hover:bg-secondary active:bg-secondary-hover'
+      }`}
+    >
+      <btn.icon className="w-6 h-6" />
+    </Link>
+  );
+};
+
 export const NavigationBar: React.FC = () => {
+  const defaultButtons: DefaultButton[] = [
+    {
+      label: 'Create Guild',
+      route: '/guilds/create',
+      icon: HiPlus,
+    },
+    {
+      label: 'Explore',
+      route: '/explore',
+      icon: FaCompass,
+    },
+  ];
+
   return (
     <>
       <div className="relative w-16 h-full"></div>
 
-      <div className="fixed left-0 w-16 h-full top-10 bg-navigation-bar"></div>
+      <div className="fixed left-0 w-16 h-full top-10 bg-navigation-bar">
+        <div className="px-2">
+          <NavButton
+            btn={{
+              label: 'Home',
+              route: '/',
+              icon: HiHome,
+            }}
+          />
+        </div>
+
+        <div className="w-10 h-px mx-auto my-3 bg-white/20"></div>
+
+        <div className="px-2">
+          {defaultButtons.map((btn) => (
+            <NavButton btn={btn} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
