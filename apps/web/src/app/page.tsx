f4fe06214ca770c1341d9a31ebbd3a40b0753dd6
slekup <74949101+slekup@users.noreply.github.com>
2023-08-12 02:47:24 +1200

    feat: added welcome banner to home

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,14 @@ export const metadata = {
 export default function Page() {
   return (
     <>
-      <p>Home</p>
+      <div className="w-full h-full p-10">
+        <div className="p-12 text-white rounded-lg bg-gradient-to-r from-blue-800 to-rose-500">
+          <p className="text-4xl font-bold">Welcome Home, User.</p>
+          <p className="mt-2 text-lg text-white/75">
+            Manage your friends, direct messages, and bookmarks here.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
