aed4ac47b9e8ba4f3b122f39c449293ca5cfadc0
slekup <74949101+slekup@users.noreply.github.com>
2023-08-11 23:15:43 +1200

    feat: add toast and layout components

---
+++ packages/ui/src/typings/core.ts
@@ -0,0 +1,14 @@
+export enum ToastType {
+  Info = 'info',
+  Success = 'success',
+  Error = 'error',
+  Warning = 'warning',
+}
+
+export interface IToast {
+  id?: string;
+  title: string;
+  description?: string;
+  type: ToastType;
+  time?: number;
+}
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
-  treeshake: true,
-  splitting: true,
-  entry: ['src/**/*.tsx'],
+  entry: ['src/index.tsx'],
   format: ['esm'],
   dts: true,
   minify: true,
   clean: true,
+  treeshake: true,
+  splitting: true,
   external: ['react'],
   ...options,
 }));
