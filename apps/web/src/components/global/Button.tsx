085389bd150f9a6d0ed8d2a72881365115ca8b2a
slekup <74949101+slekup@users.noreply.github.com>
2023-08-12 00:14:53 +1200

    fix: layout and styling

--- apps/web/src/components/global/Button.tsx
+++ apps/web/src/components/global/Button.tsx
@@ -1,7 +1,27 @@
 'use client';
 
-import { ButtonProps, Button as UIButton } from '@odnlabs/ui';
+import React from 'react';
 
-export default function Button(props: ButtonProps) {
-  return <UIButton {...props} />;
-}
+import { ButtonProps, ToastType, Button as UIButton } from '@odnlabs/ui';
+import { addToast } from '@slices/toasts.slice';
+import { useDispatch } from 'react-redux';
+
+export const Button: React.FC<ButtonProps> = (props) => {
+  const dispatch = useDispatch();
+
+  return (
+    <UIButton
+      onClick={() =>
+        dispatch(
+          addToast({
+            title: 'Hello',
+            message: 'World',
+            type: ToastType.Success,
+            duration: 5000,
+          })
+        )
+      }
+      {...props}
+    />
+  );
+};
