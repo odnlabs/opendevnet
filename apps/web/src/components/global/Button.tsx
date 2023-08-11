9f2b28cd9dfd675fa9b84a964f67854c81b6e06b
slekup <74949101+slekup@users.noreply.github.com>
2023-08-12 02:41:13 +1200

    fix(style): toasts rounded border

--- apps/web/src/components/global/Button.tsx
+++ apps/web/src/components/global/Button.tsx
@@ -2,26 +2,8 @@
 
 import React from 'react';
 
-import { ButtonProps, ToastType, Button as UIButton } from '@odnlabs/ui';
-import { addToast } from '@slices/toasts.slice';
-import { useDispatch } from 'react-redux';
+import { ButtonProps, Button as UIButton } from '@odnlabs/ui';
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const dispatch = useDispatch();
-
-  return (
-    <UIButton
-      onClick={() =>
-        dispatch(
-          addToast({
-            title: 'Hello',
-            message: 'World',
-            type: ToastType.Success,
-            duration: 5000,
-          })
-        )
-      }
-      {...props}
-    />
-  );
+  return <UIButton {...props} />;
 };
