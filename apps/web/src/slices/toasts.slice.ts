3dabd4ae79f386fa51c40a7a7092ef645827f881
slekup <74949101+slekup@users.noreply.github.com>
2023-08-14 23:32:37 +1200

    style: navigation bar buttons focus

--- apps/web/src/slices/toasts.slice.ts
+++ apps/web/src/slices/toasts.slice.ts
@@ -1,25 +1,25 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
-import { Toast } from '@typings/core';
+import { IToast } from '@odnlabs/ui';
 
 import { RootState } from 'src/store';
 
-const initialState: Toast[] = [];
+const initialState: IToast[] = [];
 
-const history: Toast[] = [];
+const history: IToast[] = [];
 
 const toastsSlice = createSlice({
   name: 'toasts',
   initialState,
   reducers: {
-    addToast: (state, action: PayloadAction<Toast>) => {
+    addToast: (state, action: PayloadAction<IToast>) => {
       const newToast = {
         id: uuidv4(),
         title: action.payload.title,
         description: action.payload.description,
         type: action.payload.type,
-        time: action.payload.time,
+        time: action.payload.time ?? 5000,
       };
       state.push(newToast);
       history.push(newToast);
