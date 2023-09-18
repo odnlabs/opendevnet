038d957929c3338f69d39682d042f1b34c1792fc
slekup <74949101+slekup@users.noreply.github.com>
2023-09-19 00:30:07 +1200

    feat: developers page

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      'api.opendevnet.com',
+      'opendevnet.com',
+      'localhost',
+      'avatars.githubusercontent.com',
+    ],
+  },
   reactStrictMode: true,
   transpilePackages: ['@odnlabs/ui'],
 };
--- apps/site/src/app/about/page.tsx
+++ apps/site/src/app/about/page.tsx
@@ -1,9 +1,16 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'About | Open Dev Net',
 };
 
 export default function About() {
-  return <div className="">About</div>;
+  return (
+    <div className="">
+      <Link href="/about/devs" className="link">
+        Developers
+      </Link>
+    </div>
+  );
 }
