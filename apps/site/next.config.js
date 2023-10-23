ce705cd6e34dde1a675a0fe9f1f88d3e0aab2891
slekup <74949101+slekup@users.noreply.github.com>
2023-10-23 14:43:33 +1300

    build: set up docker configuration with nx for site and web apps

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,10 +1,22 @@
+const withMDX = require('@next/mdx')();
+const { join } = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@odnlabs/ui', '@odnlabs/utils'],
+  transpilePackages: ['api-client', '@odnlabs/ui', '@odnlabs/utils'],
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // distDir: '../../dist/apps/site/.next', // commented out, because it should not leave the project directory
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: join(__dirname, '../../'),
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ['opendevnet.com', 'avatars.githubusercontent.com'],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
--- run.sh
+++ run.sh
@@ -3,33 +3,33 @@
 # Function to update Docker images
 function update() {
   echo "Updating docker services..."
-  docker-compose -f "$COMPOSE_FILE" down --rmi all
+  docker compose -f "$COMPOSE_FILE" down --rmi all
   git pull
-  docker-compose -f "$COMPOSE_FILE" up --build -d
+  docker compose -f "$COMPOSE_FILE" up --build -d
 }
 
 # Function to restart Docker containers
 function restart() {
   echo "Restarting docker services..."
-  docker-compose -f "$COMPOSE_FILE" restart
+  docker compose -f "$COMPOSE_FILE" restart
 }
 
 # Function to stop Docker containers
 function stop() {
   echo "Stopping docker services..."
-  docker-compose -f "$COMPOSE_FILE" down
+  docker compose -f "$COMPOSE_FILE" down
 }
 
 # Function to start Docker containers
 function start() {
   echo "Starting docker services..."
-  docker-compose -f "$COMPOSE_FILE" up --build -d
+  docker compose -f "$COMPOSE_FILE" up --build -d
 }
 
 # Function to view Docker container logs
 function logs() {
   echo "Showing logs for docker services..."
-  docker-compose -f "$COMPOSE_FILE" logs -t -f --tail 1000
+  docker compose -f "$COMPOSE_FILE" logs -t -f --tail 1000
 }
 
 # Check if the environment argument is specified
@@ -40,9 +40,9 @@ fi
 
 # Set the environment file based on the argument
 if [ "$1" == "prod" ]; then
-  COMPOSE_FILE="docker-compose.production.yml"
+  COMPOSE_FILE="docker compose.production.yml"
 elif [ "$1" == "dev" ]; then
-  COMPOSE_FILE="docker-compose.development.yml"  # Adjust the filename for development, if necessary
+  COMPOSE_FILE="docker compose.development.yml"  # Adjust the filename for development, if necessary
 else
   echo "Unknown environment '$1'. Please use 'prod' or 'dev'."
   exit 1
