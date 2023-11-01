da8c43bb6e6b23a03c8aa58bb4584a5e73995927
slekup <74949101+slekup@users.noreply.github.com>
2023-11-02 06:08:30 +1300

    refactor: remove curly braces

--- apps/site/src/utils/apiClient.ts
+++ apps/site/src/utils/apiClient.ts
@@ -1,12 +1,10 @@
 import { Client } from '@odnlabs/api-client';
 
-if (!process.env.PUBLIC_API_URL) {
+if (!process.env.PUBLIC_API_URL)
   throw new Error('API_URL environment variable is not set');
-}
 
-if (!process.env.PUBLIC_WEB_URL) {
+if (!process.env.PUBLIC_WEB_URL)
   throw new Error('WEB_URL environment variable is not set');
-}
 
 const client = new Client({
   baseApiUrl: process.env.PUBLIC_API_URL,
