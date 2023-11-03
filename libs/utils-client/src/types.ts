fa2bfcb9fe6ccf2bd3c76260a13704f06717d2b9
slekup <74949101+slekup@users.noreply.github.com>
2023-11-04 00:47:37 +1300

    chore: remove next-mdx-remote from utils-client

--- libs/utils-client/src/types.ts
+++ libs/utils-client/src/types.ts
@@ -1,4 +1,26 @@
-import { MDXRemoteSerializeResult } from 'next-mdx-remote';
+/**
+ * Represents the return value of a call to serialize().
+ */
+export type MDXRemoteSerializeResult<
+  TScope = Record<string, unknown>,
+  TFrontmatter = Record<string, unknown>,
+> = {
+  /**
+   * The compiledSource, generated from next-mdx-remote/serialize.
+   */
+  compiledSource: string;
+  /**
+   * An arbitrary object of data which will be supplied to the MDX.
+   *
+   * For example, in cases where you want to provide template variables to the MDX, like `my name is {name}`,
+   * you could provide scope as `{ name: "Some name" }`.
+   */
+  scope: TScope;
+  /**
+   * If parseFrontmatter was set to true, contains any parsed frontmatter found in the MDX source.
+   */
+  frontmatter: TFrontmatter;
+};
 
 export interface Slug {
   title: string;
