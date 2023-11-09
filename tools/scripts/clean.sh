92bee00e38bee18a688f20dc4b09ff37d140ca06
slekup <74949101+slekup@users.noreply.github.com>
2023-11-10 00:12:16 +1300

    build: update clean script

--- tools/scripts/clean.sh
+++ tools/scripts/clean.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-echo -e "${BLUE}Cleaning up...${RESET}"
+echo -e "${BLUE}Cleaning up workspace...${RESET}"
 
-echo -e "${RED}REMOVE${RESET} .nx folder"
+echo -e "${RED}REMOVE${RESET} .nx directory"
 rm -rf .nx
 
-echo -e "${RED}REMOVE${RESET} dist folder"
+echo -e "${RED}REMOVE${RESET} root dist directory"
 rm -rf dist
 
-echo -e "${RED}REMOVE${RESET} tmp folder"
+echo -e "${RED}REMOVE${RESET} library dist directories"
+rm -rf libs/**/dist
+
+echo -e "${RED}REMOVE${RESET} tmp directory"
 rm -rf tmp
 
-echo -e "${RED}REMOVE${RESET} bazel output folders"
+echo -e "${RED}REMOVE${RESET} bazel output directories"
 rm -rf bazel-*
+
+echo -e "${RED}REMOVE${RESET} next.js output directories"
+rm -rf apps/**/.next
 
 echo ""
 echo -e "${GREEN}✅ Successfully cleaned up the workspace!${RESET}"
