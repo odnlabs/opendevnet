cadb6d1ebd625bef7649a52069978612471cb6a5
slekup <74949101+slekup@users.noreply.github.com>
2023-11-10 04:34:27 +1300

    fix: renamed missed variables and files

--- apps/web-client/src/utils/apiClient.ts
+++ apps/web-client/src/utils/apiClient.ts
@@ -4,13 +4,13 @@ if (!process.env.PUBLIC_API_URL) {
   throw new Error('API_URL environment variable is not set');
 }
 
-if (!process.env.PUBLIC_WEB_URL) {
-  throw new Error('WEB_URL environment variable is not set');
+if (!process.env.PUBLIC_WEB_CLIENT_URL) {
+  throw new Error('WEB_CLIENT_URL environment variable is not set');
 }
 
 const client = new Client({
   baseApiUrl: process.env.PUBLIC_API_URL,
-  baseWebUrl: process.env.PUBLIC_WEB_URL,
+  baseWebUrl: process.env.PUBLIC_WEB_CLIENT_URL,
 });
 
 export default client;
