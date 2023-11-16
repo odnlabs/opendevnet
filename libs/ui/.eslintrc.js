e6642e337ded7095813baedbaaab160d3cd515ad
slekup <74949101+slekup@users.noreply.github.com>
2023-11-16 18:17:52 +1300

    feat(eslint): replace custom config lib for eslint-config-slekup

--- libs/ui/.eslintrc.js
+++ libs/ui/.eslintrc.js
@@ -1,22 +1,3 @@
-const path = require('path');
-
 module.exports = {
-  root: true,
-  extends: ['odn'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-  },
-  ignorePatterns: ['tsup.config.ts', 'dist/'],
-  rules: {
-    'jsdoc/require-jsdoc': 0,
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: [path.resolve(__dirname, 'tsconfig.json')],
-      },
-      node: true,
-    },
-  },
+  extends: ['slekup/library'],
 };
--- libs/utils-server/.eslintrc.js
+++ libs/utils-server/.eslintrc.js
@@ -1,19 +1,3 @@
-const path = require('path');
-
 module.exports = {
-  root: true,
-  extends: ['odn'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-  },
-  ignorePatterns: ['tsup.config.ts', 'dist/'],
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: [path.resolve(__dirname, 'tsconfig.json')],
-      },
-      node: true,
-    },
-  },
+  extends: ['slekup/library'],
 };
