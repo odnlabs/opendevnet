7b7f2e21c0e68e351e39cbab29d95e53dd942623
slekup <74949101+slekup@users.noreply.github.com>
2023-11-20 01:14:00 +1300

    ci: run api in offline mode

--- tools/scripts/setup.sh
+++ tools/scripts/setup.sh
@@ -31,44 +31,14 @@ function setup_prod_env() {
   if [ -e .env.production ]; then
     echo -e "${YELLOW}WARNING${RESET} .env.production already exists"
   else
-    echo "Setting environment variables in .env.production"
-    # Global
-    echo "ENVIRONMENT=production" >> .env.production
-    echo "DEBUG=true" >> .env.production
-    # Postgres Docker Image
-    echo "POSTGRES_PASSWORD=examplepass" >> .env.production
-    # PGAdmin Docker Image
-    echo "PGADMIN_DEFAULT_EMAIL=admin@opendevnet.com" >> .env.production
-    echo "PGADMIN_DEFAULT_PASSWORD=examplepass" >> .env.production
-    # All Clients
-    echo "PUBLIC_API_URL=https://opendevnet.com/api" >> .env.production
-    echo "PUBLIC_WS_URL=ws://opendevnet.com/ws" >> .env.production
-    # Web Client
-    echo "LOCAL_API_URL=http://backend:5000/api" >> .env.production
-    echo "LOCAL_WS_URL=ws://backend:5000/ws" >> .env.production
-    echo "PUBLIC_WEBSITE_URL=https://opendevnet.com" >> .env.production
-    echo "PUBLIC_WEB_CLIENT_URL=https://opendevnet.com/app" >> .env.production
-    echo "PUBLIC_INTERNAL_DOCS_URL=https://opendevnet.com/internal-docs" >> .env.production
-    # API
-    echo "POSTGRES_HOST=postgres" >> .env.production
-    echo "POSTGRES_PORT=6500" >> .env.production
-    echo "POSTGRES_USER=admin" >> .env.production
-    echo "POSTGRES_DB=opendevnet" >> .env.production
-    echo "DATABASE_URL=postgresql://admin:examplepass@postgres:6500/opendevnet?schema=public" >> .env.production
-    echo "CLIENT_ORIGIN=http://localhost:4000" >> .env.production
-    echo "REDIS_URL=redis://127.0.0.1:6379/" >> .env.production
-    echo "ACCESS_TOKEN_PRIVATE_KEY=secret" >> .env.production
-    echo "ACCESS_TOKEN_PUBLIC_KEY=secret" >> .env.production
-    echo "ACCESS_TOKEN_EXPIRED_IN=15m" >> .env.production
-    echo "ACCESS_TOKEN_MAXAGE=15m" >> .env.production
-    echo "REFRESH_TOKEN_PRIVATE_KEY=secret" >> .env.production
-    echo "REFRESH_TOKEN_PUBLIC_KEY=secret" >> .env.production
-    echo "REFRESH_TOKEN_EXPIRED_IN=60m" >> .env.production
-    echo "REFRESH_TOKEN_MAXAGE=60" >> .env.production
+    echo "Setting up environment variables in .env.production"
+    cp ./tools/scripts/data/.env.example.production ./.env.production || true
   fi
 
   rm ./api/.env || true
   ln -s ../.env.production ./api/.env || true
+
+  exit 0
 
   if [ "$env" == "api" ]; then
     packages=("build-essential" "pkg-config" "libssl-dev")
@@ -125,40 +95,8 @@ function setup_dev_env() {
   if [ -e .env.local ]; then
     echo -e "${YELLOW}WARNING${RESET} .env.local already exists"
   else
-    echo "Setting environment variables in .env.local"
-    # Global
-    echo "ENVIRONMENT=development" >> .env.local
-    echo "DEBUG=true" >> .env.local
-    # Postgres Docker Image
-    echo "POSTGRES_PASSWORD=examplepass" >> .env.local
-    # PGAdmin Docker Image
-    echo "PGADMIN_DEFAULT_EMAIL=admin@opendevnet.com" >> .env.local
-    echo "PGADMIN_DEFAULT_PASSWORD=examplepass" >> .env.local
-    # All Clients
-    echo "PUBLIC_API_URL=http://localhost:5000/api" >> .env.local
-    echo "PUBLIC_WS_URL=ws://localhost:5000/ws" >> .env.local
-    # Web Client
-    echo "LOCAL_API_URL=http://localhost:5000/api" >> .env.local
-    echo "LOCAL_WS_URL=ws://localhost:5000/ws" >> .env.local
-    echo "PUBLIC_WEBSITE_URL=http://localhost:4000" >> .env.local
-    echo "PUBLIC_WEB_CLIENT_URL=http://localhost:4100/app" >> .env.local
-    echo "PUBLIC_INTERNAL_DOCS_URL=http://localhost:4200/internal-docs" >> .env.local
-    # API
-    echo "POSTGRES_HOST=127.0.0.1" >> .env.local
-    echo "POSTGRES_PORT=6500" >> .env.local
-    echo "POSTGRES_USER=admin" >> .env.local
-    echo "POSTGRES_DB=opendevnet" >> .env.local
-    echo "DATABASE_URL=postgresql://admin:password123@localhost:6500/opendevnet?schema=public" >> .env.local
-    echo "CLIENT_ORIGIN=http://localhost:4000" >> .env.local
-    echo "REDIS_URL=redis://127.0.0.1:6379/" >> .env.local
-    echo "ACCESS_TOKEN_PRIVATE_KEY=secret" >> .env.local
-    echo "ACCESS_TOKEN_PUBLIC_KEY=secret" >> .env.local
-    echo "ACCESS_TOKEN_EXPIRED_IN=15m" >> .env.local
-    echo "ACCESS_TOKEN_MAXAGE=15m" >> .env.local
-    echo "REFRESH_TOKEN_PRIVATE_KEY=secret" >> .env.local
-    echo "REFRESH_TOKEN_PUBLIC_KEY=secret" >> .env.local
-    echo "REFRESH_TOKEN_EXPIRED_IN=60m" >> .env.local
-    echo "REFRESH_TOKEN_MAXAGE=60" >> .env.local
+    echo "Setting up environment variables in .env.local"
+    cp ./tools/scripts/data/.env.example.local ./.env.local || true
   fi
 
   rm ./api/.env || true
