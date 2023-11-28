55d1ea280727faf653c8a7ac0d41239240ab59ba
Slekup <74949101+slekup@users.noreply.github.com>
2023-11-28 14:05:26 +1300

    test: utils-server config and mdx api (#223)
    * test(utils-server): config validation
    
    * test(utils-server): verify frontmatter function
    
    * test(utils-server): more mdx api tests
    
    * fix(utils-server): get slugs test
    
    * fix(utils-server): url regex for config test

--- libs/utils-server/src/config.ts
+++ libs/utils-server/src/config.ts
@@ -1,15 +1,48 @@
-export default {
+interface Config {
+  name: string;
+  defaultAvatar: string;
+  social: {
+    github: string;
+    linkedin: string;
+    youtube: string;
+    email: string;
+  };
+  api: string;
+  ws: string;
+  website: string;
+  webApp: string;
+  internalDocs: string;
+}
+
+const requiredEnvVars = [
+  'PUBLIC_API_URL',
+  'PUBLIC_WS_URL',
+  'PUBLIC_WEBSITE_URL',
+  'PUBLIC_WEB_CLIENT_URL',
+  'PUBLIC_INTERNAL_DOCS_URL',
+];
+
+for (const envVar of requiredEnvVars) {
+  if (!process.env[envVar]) {
+    throw new Error(`${envVar} is not set`);
+  }
+}
+
+const config: Config = {
   name: 'Open Dev Net',
   defaultAvatar: '/default-avatar.png',
   social: {
-    github: 'https://github.com/odnlabs',
-    linkedin: 'https://www.linkedin.com/company/opendevnet',
-    youtube: 'https://www.youtube.com/@odnlabs',
+    // NGINX will redirect these to the correct URLs
+    github: '/github',
+    linkedin: '/linkedin',
+    youtube: '/youtube',
     email: 'mailto:slekupvimplyrataqq@protonmail.com',
   },
-  api: process.env.PUBLIC_API_URL,
-  ws: process.env.PUBLIC_WS_URL,
-  website: process.env.PUBLIC_WEBSITE_URL,
-  webApp: process.env.PUBLIC_WEB_CLIENT_URL,
-  internalDocs: process.env.PUBLIC_INTERNAL_DOCS_URL,
+  api: process.env.PUBLIC_API_URL as string,
+  ws: process.env.PUBLIC_WS_URL as string,
+  website: process.env.PUBLIC_WEBSITE_URL as string,
+  webApp: process.env.PUBLIC_WEB_CLIENT_URL as string,
+  internalDocs: process.env.PUBLIC_INTERNAL_DOCS_URL as string,
 };
+
+export default config;
--- libs/utils-server/src/mdxApi.test.ts
+++ libs/utils-server/src/mdxApi.test.ts
@@ -1,22 +1,62 @@
-import { expect, test } from 'vitest';
-import { getSlugPosition, removePos } from './mdxApi';
+import { describe, expect, it } from 'vitest';
+import {
+  getSlugPosition,
+  getSlugs,
+  removePos,
+  verifyFrontmatter,
+} from './mdxApi';
 
-test('gets slug position', () => {
-  expect(getSlugPosition('01-foo-bar')).toEqual(1);
-  expect(getSlugPosition('50-foo-bar')).toEqual(50);
-  expect(() => getSlugPosition('00-foo-bar')).toThrowError();
-  expect(() => getSlugPosition('01')).toThrowError();
-  expect(() => getSlugPosition('-')).toThrowError();
-  expect(() => getSlugPosition('foo-bar')).toThrowError();
-  expect(() => getSlugPosition('-foo-bar')).toThrowError();
-});
+describe('mdxApi', () => {
+  it('gets slug position', () => {
+    expect(getSlugPosition('01-foo-bar')).toEqual(1);
+    expect(getSlugPosition('50-foo-bar')).toEqual(50);
+    expect(() => getSlugPosition('00-foo-bar')).toThrowError();
+    expect(() => getSlugPosition('01')).toThrowError();
+    expect(() => getSlugPosition('-')).toThrowError();
+    expect(() => getSlugPosition('foo-bar')).toThrowError();
+    expect(() => getSlugPosition('-foo-bar')).toThrowError();
+  });
 
-test('removes slug position', () => {
-  expect(removePos('01-foo-bar')).toEqual('foo-bar');
-  expect(removePos('50-foo-bar')).toEqual('foo-bar');
-  expect(() => removePos('00-foo-bar')).toThrowError();
-  expect(() => removePos('01')).toThrowError();
-  expect(() => removePos('-')).toThrowError();
-  expect(() => removePos('foo-bar')).toThrowError();
-  expect(() => removePos('-foo-bar')).toThrowError();
+  it('removes slug position', () => {
+    expect(removePos('01-foo-bar')).toEqual('foo-bar');
+    expect(removePos('50-foo-bar')).toEqual('foo-bar');
+    expect(() => removePos('00-foo-bar')).toThrowError();
+    expect(() => removePos('01')).toThrowError();
+    expect(() => removePos('-')).toThrowError();
+    expect(() => removePos('foo-bar')).toThrowError();
+    expect(() => removePos('-foo-bar')).toThrowError();
+  });
+
+  it('validates frontmatter', () => {
+    expect(() => {
+      verifyFrontmatter(
+        '/',
+        {
+          title: 'title',
+        },
+        [
+          ['title', 'string'],
+          ['position', 'number'],
+        ]
+      );
+    }).toThrowError();
+
+    expect(() => {
+      verifyFrontmatter(
+        '/',
+        {
+          description: 5,
+        },
+        [['description', 'string']]
+      );
+    }).toThrowError();
+  });
+
+  it('gets slugs', async () => {
+    await expect(
+      getSlugs('../../no-docs/dir-that-doest-exist')
+    ).rejects.toThrowError();
+
+    await expect(getSlugs('../../docs/internal')).resolves.not.toThrowError();
+  });
 });
