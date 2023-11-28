57194e515bda9ace5d8121eace64816924087d96
Slekup <74949101+slekup@users.noreply.github.com>
2023-11-28 14:02:42 +1300

    chore: initialize vitest (#224)
    * chore(utils-client): init vitest
    
    * chore(ui): init vitest
    
    * chore(api-client): init vitest
    
    * fix: vitest shared config eslint import error

---
+++ libs/api-client/vitest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig, mergeConfig } from 'vitest/config';
+import sharedConfig from '../../vitest.shared';
+
+export default mergeConfig(
+  sharedConfig,
+  defineConfig({
+    test: {
+      environment: 'node',
+      coverage: {
+        reportsDirectory: './../../coverage/api-client',
+      },
+    },
+  })
+);
